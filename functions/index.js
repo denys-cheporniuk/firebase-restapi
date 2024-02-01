25e4dee95890e8b88918691d6e8cb35db4362482
Denys Cheporniuk <denys.cheporniuk@gmail.com>
2024-02-01 11:16:47 +0200

    init firebase project

---
+++ functions/index.js
@@ -0,0 +1,8 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+exports.app = functions.https.onRequest(app);
